cc4e2cae2bac33e6385273736b6b722a6cf6bb4c
kirank860 <kkiru889@gmail.com>
2024-03-25 17:18:59 +0530

    changes again

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
   return (
     <div name="home" className="w-full h-screen bg-[#0a192f]">
       {/* container */}
-      <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
+      <div className="max-w-[1000px] mx-auto  px-8 flex flex-col justify-center h-full">
         <p className="text-pink-600">Hi, My name is </p>
         <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
           Kiran k
--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -10,7 +10,7 @@ import node from "../assets/node.png";
 
 const Skills = () => {
   return (
-    <div name="skills" className="w-full h-screen bg-[#0a192f] text-gray-300">
+    <div name="skills" className="w-full mt-[-50px] md:mt-[-200px] md:h-screen bg-[#0a192f] text-gray-300">
       {/* container */}
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
         <div>
