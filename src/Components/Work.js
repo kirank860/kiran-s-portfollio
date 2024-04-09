717ef725e9f7c00376800ad33761c7419c275d5f
kirank860 <kkiru889@gmail.com>
2024-04-09 12:54:18 +0530

    added one more project

--- src/Components/Work.js
+++ src/Components/Work.js
@@ -1,10 +1,11 @@
 import React from "react";
 import working from "../assets/projects/workImg.jpeg";
 import realestate from "../assets/projects/realestate.jpg";
-import malabar from "../assets/projects/malabar.png"
-import aljamia from "../assets/projects/aljamia.png"
-import ecommerce from "../assets/projects/ecomerce.png"
-import deconqustia from "../assets/projects/deconqustia.png"
+import malabar from "../assets/projects/malabar.png";
+import aljamia from "../assets/projects/aljamia.png";
+import ecommerce from "../assets/projects/ecomerce.png";
+import deconqustia from "../assets/projects/deconqustia.png";
+import fooda_app from "../assets/projects/food-app.png"
 const Work = () => {
   return (
     <div name="works" className="w-full md:h-screen text-gray-300 bg-[#0a192f]">
@@ -17,7 +18,30 @@ const Work = () => {
         </div>
         {/* container */}
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
-            {/* grid items */}
+          {/* grid items */}
+          <div
+            style={{ backgroundImage: `url(${fooda_app})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* hover effect */}
+            <div className="opacity-0  group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                ejs Application
+              </span>
+              <div className="pt-8 text-center">
+                <a href="https://food-app-peach.vercel.app/">
+                  <button className="text-center py-3 px-4 m-2 rounded-lg bg-white text-gary-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="https://github.com/kirank860/food-app/">
+                  <button className="text-center py-3 px-4 m-2 rounded-lg bg-white text-gary-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
           <div
             style={{ backgroundImage: `url(${malabar})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
@@ -48,7 +72,7 @@ const Work = () => {
             {/* hover effect */}
             <div className="opacity-0  group-hover:opacity-100">
               <span className="text-2xl font-bold text-white tracking-wider">
-             Next.js Application
+                Next.js Application
               </span>
               <div className="pt-8 text-center">
                 <a href="https://www.aljamia.net/">
